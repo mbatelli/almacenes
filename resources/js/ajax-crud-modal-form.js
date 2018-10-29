d20e55ba20ec3d8f70536561e065d8eb397afdcf
Esteban Pastor <epastor@iMacPastor.local>
2018-10-29 12:20:00 -0300

    reubicacion del js en el lugar correcto

--- resources/js/ajax-crud-modal-form.js
+++ resources/js/ajax-crud-modal-form.js
@@ -1,8 +1,3 @@
-function CloseModal(id){
-    $('#'+id+' .close').click();
-    $('.modal-backdrop').remove();
-    $('body').removeClass('modal-open');
-}
 
 function doResponseCommand(data){
     if(data.reload)
@@ -18,7 +13,6 @@ $(document).on('click', 'a.page-link', function (event) {
     ajaxLoad($(this).attr('href'));
 });
 $(document).on('submit', '#modalForm form#frmTbl', function (event) {
-//$(document).on('click', 'button.btn-primary', function (event) {
     event.preventDefault();
     var form = $(this);
     var data = new FormData(form[0]);
@@ -40,18 +34,6 @@ $(document).on('submit', '#modalForm form#frmTbl', function (event) {
             } else {
                 $('#modalForm').modal('hide');
                 doResponseCommand(data);
-/*
-                if(data.reload) {
-                    window.location.href = window.location.href;
-                } else if(typeof data.redirect_url != 'undefined') {
-                    $('#modalForm').modal('hide');
-                    ajaxLoad(data.redirect_url);
-                } else if(typeof data.table_refresh != 'undefined') {
-                    $('#' + data.table_refresh).DataTable().ajax.reload();
-//                    $('#modalForm').modal('hide');
-//                    setTimeout(function () {$('#modalForm .close').click();}, 500);
-                }
-*/
             }
         },
         error: function (xhr, textStatus, errorThrown) {
@@ -84,10 +66,9 @@ function ajaxDelete(filename, token, content) {
         data: {_method: 'DELETE', _token: token},
         url: filename,
         success: function (data) {
-//            $('#modalDelete').modal("hide");
-//            $('.modal-backdrop').fadeOut();
-//            $("#" + content).html(data);
-//            $('.loading').hide();
+            $('#modalDelete').modal("hide");
+            $("#" + content).html(data);
+            $('.loading').hide();
             doResponseCommand(data);
         },
         error: function (xhr, status, error) {
